bca59cde6479afbe88f928071b402144b5d67c63
Gabriel Arias <gabrielariaschinchilla@gmail.com>
2026-06-14 10:04:43 -0600

    Create wordlist.py

---
+++ hangman/wordlist.py
@@ -0,0 +1,8 @@
+words = ("apple","beach","castle","dragon","elephant","forest","guitar","harbor","island","jungle","kitchen","lantern",
+         "mountain","notebook","ocean","penguin","question","rainbow","sunset","turtle","umbrella","village","whisper",
+         "x-ray","yellow","zebra","anchor","bridge","candle","desert","engine","feather","garden","hammer","igloo","jacket",
+         "kangaroo","ladder","mirror","napkin","orange","pencil","quilt","river","saddle","tiger","unicorn","violin","window",
+         "yogurt","zipper","balloon","camera","diamond","eagle","flame","glove","horizon","insect","jewel","kettle","lemon","mango",
+         "nest","oyster","puzzle","queen","rocket","star","tunnel","utensil","valley","wagon","falcon","yacht","zone","arrow",
+         "basket","cabin","drum","empire","fountain","goblin","helmet","ivory","jewelry","knight","lighthouse","meadow","needle",
+         "oasis","pirate","quiver","ribbon","statue","temple","ukulele","vase","whale","xylophone","yarn","zeppelin","panther")
